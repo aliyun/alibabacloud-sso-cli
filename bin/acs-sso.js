807d80d79c18ef70355002807684151a7ee3173d
Jackson Tian <shyvo1987@gmail.com>
2024-10-15 14:58:01 +0800

    add doc command

--- bin/acs-sso.js
+++ bin/acs-sso.js
@@ -8,6 +8,7 @@ import CLI from '../lib/cli.js';
 import Login from '../commands/login.js';
 import Configure from '../commands/configure.js';
 import Profile from '../commands/profile.js';
+import Doc from '../commands/doc.js';
 
 const pkgPath = new URL('../package.json', import.meta.url);
 const pkg = loadJSONSync(fileURLToPath(pkgPath));
@@ -17,4 +18,5 @@ const app = new CLI('acs-sso', pkg.version, 'Alibaba Cloud SSO CLI');
 app.registerCommand(Login);
 app.registerCommand(Configure);
 app.registerCommand(Profile);
+app.registerCommand(Doc);
 app.run(argv);
